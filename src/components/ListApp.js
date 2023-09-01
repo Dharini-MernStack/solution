8f6773580ed365e9df0502f49d0c56aa2e85948d
Dharini S <dharinisrinivasan.94@gmail.com>
2023-09-01 14:22:45 +0530

    Update ListApp.js

--- src/components/ListApp.js
+++ src/components/ListApp.js
@@ -4,8 +4,15 @@ class ListApp extends Component {
   state = {
     list: []
   };
+  componentDidMount = () => {
+    const getData = this.props.read && this.props.read("shopping_list");
+    this.setState({
+      list: getData ? getData : []
+    });
+  };
   componentDidUpdate = prevProps => {
     if (prevProps.task !== this.props.task && this.props.task !== "") {
+      // Update list
       const updatedList = [
         {
           id: new Date().getTime(),
@@ -16,6 +23,7 @@ class ListApp extends Component {
       this.setState({
         list: updatedList
       });
+      this.props.save && this.props.save("shopping_list", updatedList);
     }
   };
   remove = id => {
@@ -23,6 +31,7 @@ class ListApp extends Component {
     this.setState({
       list: newList
     });
+    this.props.save && this.props.save("shopping_list", newList);
   };
   render = () =>
     this.props.children({list: this.state.list, remove: this.remove});
